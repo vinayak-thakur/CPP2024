7d61cb6413adfe926d79c061e3eb8d695cb54063
Vinayak Thakur <vinayak.cmrit@gmail.com>
2024-04-23 00:41:02 +0530

    array1 done

--- Arrays_timecomp_spacecomp/arrays1.cpp
+++ Arrays_timecomp_spacecomp/arrays1.cpp
@@ -100,32 +100,66 @@ using namespace std ;
 // }
 // ***************//
 //Linear Search in Array 
-// int main() {
-//     int n ;
-//     cout<<"enter the number of elements " ;
-//     cin>>n ;
-//     int arr[100] ;
 
-//     cout<<"enter the values in array"<<endl; // TAKING THE INPUT IN ARRAY FROM USER 
-//     for(int i = 0; i<n;i++){
-//         cin>>arr[i] ;
-//     }
-//      for(int i = 0; i<n;i++){    // PRINTING THE CONTENTS OF THE ARRAY 
-//         cout<<arr[i]<<" " ;
-//     }
-//     int key ;
-//         cout<<"enter the key" ;
-//         cin>>key ;
-//     // checkling each item of array if it equals the search element
-//     for (int i = 0 ; i<n;i++){
-//         if(arr[i]==key){
-//             cout<<"key found\n"<<i ;
+// #include <iostream>
+// using namespace std;
+// void checkkey(int array[], int size,int key){
+//     bool flag = 0;
+//     for (int i = 0 ; i <size; i++){
+//         if (array[i]==key){
+           
+//              flag= 1;
 //         }
         
 //     }
+//      if(flag==1){
+//       cout<<"Present";
+      
+//   }
+//   else{
+//       cout<<"absent";
+//   }
     
 // }
+  
+// int main() {
+//   using namespace std;
+//   int size=5;
+//   int key;
+ 
+//   int array[] = {2,6,8,12,3} ;
+//   cout<<"enter the key to search" ;
+//   cin>>key;
+//   checkkey(array,size,key);
+ 
+   
+//   return 0 ;
+//   }
+// alternate code to search in an array
+// bool chekk(int arr[],int size,int key){
+//     for (int i = 0 ;i<size; i++){
+//         if(arr[i]==key){
+//             return true;
+//             break;
+//         }
+//     }
+//     return false;
+// }
+//   int main (){
+//     int key ;
+//     int size= 5 ;
+//     int arr[] = { 12, 14, 19,23,29} ;
+//     cout<<"enter the key ";
+//     cin>>key;
+//     if(chekk(arr,size,key)){
+//         cout<<"Present" ;
+//     }
+//     else {
+//         cout<<"Absent";
+//     }
 
+//   }
+ 
 // COUNT ZERO AND ONES IN A ARRAY 
 // int main() {
 //     int n ;
@@ -158,26 +192,122 @@ using namespace std ;
 
 // *************************// 
 // max number in an array //
+// #include <iostream>
+// #include<climits>
+// using namespace std;
+// int main() {
+//     int n ;
+//     cout<<"enter the number of elements " ;
+//     cin>>n ;
+//     int arr[100] ;
+
+//     cout<<"enter the values in array"<<endl; // TAKING THE INPUT IN ARRAY FROM USER 
+//     for(int i = 0; i<n;i++){
+//         cin>>arr[i] ;
+//     }
+//      for(int i = 0; i<n;i++){    // PRINTING THE CONTENTS OF THE ARRAY 
+//         cout<<arr[i]<<" " ;
+//     }
+//     int max = INT_MIN ;
+//     for(int i= 0 ; i<n;i++){
+//     if(arr[i]>max){
+//         max=arr[i] ;
+//     }
+
+//     }
+//     cout<<endl;
+//     cout<<max<<" " ;
+    
+// }
+// MIN NUMBER IN A ARRAY using i _max and in_min
+// #include <iostream>
+// #include<climits>
+// using namespace std;
+// int main() {
+//     int n ;
+//     cout<<"enter the number of elements " ;
+//     cin>>n ;
+//     int arr[100] ;
+
+//     cout<<"enter the values in array"<<endl; // TAKING THE INPUT IN ARRAY FROM USER 
+//     for(int i = 0; i<n;i++){
+//         cin>>arr[i] ;
+//     }
+//      for(int i = 0; i<n;i++){    // PRINTING THE CONTENTS OF THE ARRAY 
+//         cout<<arr[i]<<" " ;
+//     }
+//     int min = INT_MAX ;
+//     for(int i= 0 ; i<n;i++){
+//     if(arr[i] < min){
+//         min=arr[i] ;
+//     }
+
+//     }
+//     cout<<endl;
+//     cout<<min<<" " ;
+    
+// }
+// PRINT EXTREMES OF AN ARRAY 
+// int main() {
+//     int arr[] = {23,45,11,17,19,27} ;
+//     int size = 6;
+//     int start = 0;
+//     int end = size-1;
+//     while(true){
+//         if(start>end)
+//         break;
+//        else if(start==end){   // if this conditon is not inckuded than two times start or end will be printed 
+//             cout<<arr[start]<<" ";
+
+//         }
+//         else{
+//             cout<<arr[start]<<" ";
+//             cout<<arr[end]<<"  ";
+
+//         }
+//         start++;
+//         end--;
+//     }
+    
+    
+// }
+/* print the extremes of an array just the loop condition has been changed 
 int main() {
-    int n ;
-    cout<<"enter the number of elements " ;
-    cin>>n ;
-    int arr[100] ;
+    int arr[] = {23,45,11,17,19,27} ;
+    int size = 6;
+    int start = 0;
+    int end = size-1;
+    while(start<=end) // if start is less than end than inly start the loop or dont start only
+        
+        if(start==end){   // if this conditon is not inckuded than two times start or end will be printed 
+            cout<<arr[start]<<" ";
 
-    cout<<"enter the values in array"<<endl; // TAKING THE INPUT IN ARRAY FROM USER 
-    for(int i = 0; i<n;i++){
-        cin>>arr[i] ;
-    }
-     for(int i = 0; i<n;i++){    // PRINTING THE CONTENTS OF THE ARRAY 
-        cout<<arr[i]<<" " ;
-    }
-    int max =arr[0] ;
-    for(int i= 0 ; i<n;i++){
-    if(arr[i]>max){
-        max=arr[i] ;
-    }
+        }
+        else{
+            cout<<arr[start]<<" ";
+            cout<<arr[end]<<"  ";
 
+        }
+        start++;
+        end--;
     }
-    cout<<endl;
-    cout<<max<<" " ;
+    
+    
+}
+*/
+// Reverse the following array
+int main(){
+    int arr[8]= {3,4,7,9,1,2,5,6};
+    int size=8;
+ int start=0;
+ int end = size-1;
+ while(start<=end){
+    //step1;
+    swap(arr[start],arr[end]);
+    start++;
+    end--;
+ }
+ //printing array
+ for(int i =0;i<=size;i++)
+ cout<<arr[i];
 }
